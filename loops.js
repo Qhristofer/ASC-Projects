49572bf3f4076a934566e2871ef0c7eb39f8b5e3
Qhristofer <cdominguez@stuy.edu>
2018-07-10 14:50:55 -0400

    loops

---
+++ loops.js
@@ -0,0 +1,13 @@
+let foods = [["broccoli", "carrots", "beans"], ["pizza", "fried chicken"], ["icecream", "donuts"]]
+
+ for (i=0; i < foods.length; i++){
+     let b = foods[i]
+     for (i=0; i < b.length; i++){
+         console.log(b[i])
+     }
+     
+ }
+
+// for ( const item of foods ){ 
+//     console.log(item); 
+//  }
